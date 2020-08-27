93edcbc93d58e2b7bb0baa69ca0d4289ee3713b4
majiong <majiong@deepexi.comm>
2020-08-27 17:51:14 +0800

    feat: 微前端主应用

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
+  <div id="main-app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view />
+    <router-view>
+    </router-view>
+    <div id="vue-app"></div>
+    <!-- <router-view /> -->
   </div>
 </template>
 
 <style lang="less">
-#app {
+#main-app {
+  display: flex;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -18,7 +22,10 @@
 }
 
 #nav {
-  padding: 30px;
+  width: 200px;
+  background: red;
+  height: 100vh;
+  // padding: 30px;
 
   a {
     font-weight: bold;
@@ -28,5 +35,11 @@
       color: #42b983;
     }
   }
+}
+
+#vue-app{
+  height: 100vh;
+  flex-grow: 1;
+  background: blue;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -10,3 +10,20 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+import { registerMicroApps, start } from 'qiankun';
+registerMicroApps([
+  {
+    name: 'sub-app', // app name registered
+    entry: '//localhost:8099',
+    container: '#vue-app',
+    activeRule: '/active',
+  },
+  // {
+  //   name: 'vue app',
+  //   entry: { scripts: ['//localhost:7100/main.js'] },
+  //   container: '#yourContainer2',
+  //   activeRule: '/yourActiveRule2',
+  // },
+]);
+start();
